73759408dcfa99f620d336285e7add6c83238cd2
yf wang <wangyf530@gmail.com>
2025-01-02 13:47:39 +0800

    建立最新文章管理頁面架構

---
+++ back/news.php
@@ -0,0 +1,24 @@
+<?php include_once "./api/db.php";?>
+
+
+<fieldset style="width:85%; margin:auto">
+    <legend>最新文章管理</legend>
+    <!-- table.ct>(tr>th*4)+(tr>td)*4 -->
+    <table class="ct" style="width:100%">
+        <tr>
+            <th>編號</th>
+            <th style="width:50%">標題</th>
+            <th>顯示</th>
+            <th>刪除</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <button onclick="edit()">確定修改</button>
+    </div>
+</fieldset>
